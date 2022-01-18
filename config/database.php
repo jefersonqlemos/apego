7ac96bd772438689da4c5530f9cbd633969327af
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2022-01-18 15:28:21 -0300

    update 35 2022

--- config/database.php
+++ config/database.php
@@ -56,10 +56,10 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'modes' => [
+            'strict' => false,
+            /*'modes' => [
                 'ONLY_FULL_GROUP_BY',
-            ],
+            ],*/
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
