9ab65186dffb49383ccc80838d4c2f169bbb1990
jeferson lemos <jefersonquagliottolemos@hotmail.com>
2022-01-19 02:50:39 -0300

    update 43 2022

--- resources/views/produtodetalhes.blade.php
+++ resources/views/produtodetalhes.blade.php
@@ -72,7 +72,7 @@
                             @if($produto->quantidade != 0)
                                 Produto em Estoque ({{$produto->quantidade}})
                             @else
-                                Produto Fora de Estoque
+                                Produto Fora do Estoque
                             @endif
                             </span></h3>
                         
